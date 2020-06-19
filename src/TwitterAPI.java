eb051d6f8496a2daa3c0b8847ccbf254606f1521
bcakir00 <berkan.cakir@student.hu.nl>
2020-06-19 18:35:48 +0200

    Moved procedural code from main to getTweets method in TwitterAPI class

--- src/TwitterAPI.java
+++ src/TwitterAPI.java
@@ -1,12 +1,13 @@
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class TwitterAPI {
     private Twitter twitter;
     private String language;
-    private int amount;
 
     public TwitterAPI() {
         ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -20,27 +21,34 @@ public class TwitterAPI {
         twitter = TwitterFactory.getInstance();
     }
 
-    public List<Status> getTweets(String keyword) {
-        List<Status> tweets = null;
+    public List<Status> getTweets(String keyword, int limit) {
+        List<Status> tweetss = new ArrayList<>();
 
         try {
             Query query = new Query();
             query.setQuery(keyword);
             query.setLang(language);
-            query.setCount(amount);
-            tweets = twitter.search(query).getTweets();
+            query.setCount(limit);
+
+            int tweetCount = 0;
+            while(tweetCount < limit) {
+                List<Status> tweets = twitter.search(query).getTweets();
+
+                for(Status tweet : tweets) {
+                    query.setMaxId(tweet.getId());
+                    tweetss.add(tweet);
+
+                    tweetCount++;
+                }
+            }
         } catch (TwitterException e) {
             e.printStackTrace();
         }
 
-        return tweets;
+        return tweetss;
     }
 
     public void setLanguage(String language) {
         this.language = language;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
     }
 }
