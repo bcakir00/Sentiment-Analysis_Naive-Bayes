7a5f3813f499b1dad4e0df07a1b86132d098f67e
bcakir00 <berkan.cakir@student.hu.nl>
2020-06-21 09:21:16 +0200

    Add basic noSQL functionality.

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,24 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class Database {
+    MongoClient mongoClient;
+    MongoDatabase database;
+    MongoCollection collection;
+
+    public Database(String name) {
+        MongoClient mongoClient = new MongoClient();
+        database = mongoClient.getDatabase(name);
+    }
+
+    public void insertOneDocument(String collectionName, Document document) {
+        collection = database.getCollection(collectionName);
+        collection.insertOne(document);
+    }
+
+    public void closeConnection() {
+        mongoClient.close();
+    }
 }
