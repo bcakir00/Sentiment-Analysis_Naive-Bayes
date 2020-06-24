66a8acd4ac1cad68a88876a47870a4e3bbb35f61
bcakir00 <berkan.cakir@student.hu.nl>
2020-06-25 00:20:33 +0200

    Add Dashboard class

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -1,2 +1,27 @@
 public class Dashboard {
+    int tweetsChecked;
+    int tweetsGuessedCorrectly;
+
+    public Dashboard() {
+        tweetsChecked = 0;
+        tweetsGuessedCorrectly = 0;
+    }
+
+    public void printSimpleDashboard() {
+        int accuracy = calculateModelAccuracy(tweetsGuessedCorrectly, tweetsChecked);
+
+        System.out.println(accuracy);
+    }
+
+    private int calculateModelAccuracy(int correct, int total) {
+        return correct / total;
+    }
+
+    public void incrementTweetsChecked() {
+        this.tweetsChecked++;
+    }
+
+    public void incrementTweetsGuessedCorrectly() {
+        this.tweetsGuessedCorrectly++;
+    }
 }
