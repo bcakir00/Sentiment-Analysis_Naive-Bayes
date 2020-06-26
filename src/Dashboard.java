df7e1f22f59e4c742d0ca35f9f16d12e78c6355d
bcakir00 <berkan.cakir@student.hu.nl>
2020-06-26 12:26:07 +0200

    Add documentation to dashboad class

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -1,3 +1,6 @@
+/**
+ * Text based dashboard to show useful information about accuracy and amount of tweets in sentiment classes.
+ */
 public class Dashboard {
     int tweetsChecked;
     int tweetsGuessedCorrectly;
@@ -5,6 +8,9 @@ public class Dashboard {
     int neutralTweets;
     int negativeTweets;
 
+    /**
+     * Constructor, sets all initial counter to 0
+     */
     public Dashboard() {
         this.tweetsChecked = 0;
         this.tweetsGuessedCorrectly = 0;
@@ -13,12 +19,18 @@ public class Dashboard {
         this.negativeTweets = 0;
     }
 
+    /**
+     * Checks how many tweets are positive, neutral or negative and prints percentages
+     */
     public void printAccuracyDashboard() {
         double accuracy = calculateModelAccuracy(tweetsGuessedCorrectly, tweetsChecked);
 
         System.out.println("\nINFO - Accuracy: " + accuracy);
     }
 
+    /**
+     * Prints positive, neutral and negative tweets percentage respectively
+     */
     public void printDashboard() {
         double positivePercentage = (double) positiveTweets / (double)tweetsChecked;
         double neutralPercentage = (double) neutralTweets / (double)tweetsChecked;
@@ -29,26 +41,48 @@ public class Dashboard {
                 "\nNegative Tweets: " + negativePercentage);
     }
 
+    /**
+     * Calculates accuracy of AI model
+     *
+     * @param correct correct tweets
+     * @param total total tweets
+     * @return percentage
+     */
     private double calculateModelAccuracy(int correct, int total) {
         return (double) correct / (double) total;
     }
 
+    /**
+     * Increments tweetsChecked by one
+     */
     public void incrementTweetsChecked() {
         this.tweetsChecked++;
     }
 
+    /**
+     * Increments tweetsGuessedCorrectly by one
+     */
     public void incrementTweetsGuessedCorrectly() {
         this.tweetsGuessedCorrectly++;
     }
 
+    /**
+     * Increments positiveTweets by one
+     */
     public void incrementPositiveTweets() {
         this.positiveTweets++;
     }
 
+    /**
+     * Increments neutralTweets by one
+     */
     public void incrementNeutralTweets() {
         this.neutralTweets++;
     }
 
+    /**
+     * Increments negativeTweets by one
+     */
     public void incrementNegativeTweets() {
         this.negativeTweets++;
     }
