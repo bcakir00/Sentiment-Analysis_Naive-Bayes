1410e0e8ee9015f3f047ae399be58a92c6493e70
bcakir00 <berkan.cakir@student.hu.nl>
2020-06-26 14:42:33 +0200

    Add unittests for Data class

--- tests/DataTest.java
+++ tests/DataTest.java
@@ -1,5 +1,20 @@
+import org.junit.jupiter.api.Test;
+import twitter4j.Status;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DataTest {
 
+    @Test
+    void splitTrainingData() {
+        TwitterAPI api = new TwitterAPI();
+        List<Status> list = api.getTweets("test", 5);
+
+        Data data = new Data();
+        List<List<Status>> splitList = data.splitTrainingData(list);
+
+        assertEquals(1, splitList.get(1).size());
+    }
 }
